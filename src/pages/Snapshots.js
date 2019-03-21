e6273b8c9616f9c22d85e60584c8832c4858a328
Kondax <kondaxsolutions@gmail.com>
2019-03-21 15:00:30 +0000

    Remove dead code: isUrl()

--- src/pages/Snapshots.js
+++ src/pages/Snapshots.js
@@ -106,11 +106,6 @@ class Snapshots extends React.Component {
     }
   }
 
-  isUrl(s) {
-    var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
-    return regexp.test(s)
-  }
-
   render() {
     const { loading, tabs, currentTab, content } = this.state
     const { actions } = this.props
